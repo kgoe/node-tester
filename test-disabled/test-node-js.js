0c1cb044e07a0cb83259a0a45a3955ad2d8e693e
kgoe <kgoe@users.noreply.github.com>
2018-08-29 02:14:21 +0800

    Update test-node-js.js

--- test-disabled/test-node-js.js
+++ test-disabled/test-node-js.js
@@ -1,99 +1,48 @@
 var assert = require('assert');
+var async_hooks = require('async_hooks');
+var buffer = require('buffer');
+var child_process = require('child_process');
+var cluster = require('cluster');
+var crypto = require('crypto');
+var dgram = require('dgram');
+var dns = require('dns');
+var domain = require('domain');
+var events = require('events');
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var net = require('net');
+var os = require('os');
+var path = require('path');
+var perf_hooks = require('perf_hooks');
+var punycode = require('punycode');
+var querystring = require('querystring');
+var readline = require('readline');
+var repl = require('repl');
+var stream = require('stream');
+var string_decoder = require('string_decoder');
+var tls = require('tls');
+var tty = require('tty');
+var url = require('url');
+var util = require('util');
+var v8 = require('v8');
+var vm = require('vm');
+var zlib = require('zlib');
 
 describe('Node.JS Core', function() {
-
+  it('assert', function() {
+      assert.equal(typeof assert, 'object');
+  });
+  it('async_hooks', function() {
+      assert.equal(typeof async_hooks, 'object');
+  });
+  it('buffer', function() {
+      assert.equal(typeof buffer, 'object');
+  });
+  it('child_process', function() {
+      assert.equal(typeof child_process, 'object');
+  });
+  it('cluster', function() {
+      assert.equal(typeof cluster, 'object');
+  });
 });
-
-/*
-// DevDocs
-Assert
-Async Hooks
-Buffer
-Child Process
-Cluster
-Command Line Options
-Console
-Crypto
-DNS
-Domain
-ECMAScript Modules
-Errors
-Events
-File System
-Global Objects
-HTTP
-HTTP/2
-HTTPS
-Inspector
-Internationalization Support
-Miscellaneous
-Modules
-N-API
-Net
-OS
-Path
-Performance Timing API
-Process
-Punycode
-Query String
-Readline
-REPL
-Stream
-String Decoder
-Timers
-TLS/SSL
-TTY
-UDP/Datagram
-URL
-Util
-V8
-VM
-Zlib
-
-// Documentation
-Assertion Testing
-Async Hooks
-Buffer
-C++ Addons
-C/C++ Addons N-API
-Child Processes
-Cluster
-Command Line Options
-Console
-Crypto
-Debugger
-Deprecated APIs
-DNS
-ECMAScript Modules
-Errors
-Event
-File Systen
-Globals
-HTTP
-HTTP/2
-HTTPS
-Inspector
-Internationalization
-Modules
-Net
-OS
-Path
-Performance Hooks
-Process
-Punycode
-Query Strings
-Readline
-REPL
-Stream
-String Decoder
-Timers
-TLS/SSL
-Tracing
-TTY
-UDP/Datagram
-URL
-Utilities
-V8
-VM
-ZLIB
-*/
