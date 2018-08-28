416f7066dcb0ec5dca127df85f02d0caceba4008
kgoe <kgoe@users.noreply.github.com>
2018-08-29 01:47:50 +0800

    Update test-002.js

--- test-disabled/test-002.js
+++ test-disabled/test-002.js
@@ -3,3 +3,34 @@ var assert = require('assert');
 describe('Node.JS Core', function() {
 
 });
+
+/*
+// DevDocs
+// Documentation
+Assertion Testing
+Async Hooks
+Buffer
+C++ Addons
+C/C++ Addons N-API
+Child Processes
+Cluster
+Command Line Options
+Console
+Crypto
+Debugger
+Deprecated APIs
+DNS
+ECMAScript Modules
+Errors
+Event
+File Systen
+Globals
+HTTP
+HTTP/2
+HTTPS
+Inspector
+Internationali
+Modules
+Net
+
+*/
