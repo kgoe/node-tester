87ecc724161cb64a8a3fdafbb7147530584ccf9d
kelvin.espiritu <espiritu0462@gmanmi.com>
2018-08-31 10:53:44 +0800

    updated test script and property maps

---
+++ test/test.js
@@ -0,0 +1,21 @@
+/*
+if ( typeof global !== 'undefined' ) {
+  var prop = Object.getOwnPropertyNames(global);
+  var str = JSON.stringify(prop,'','\n');
+  prop.map(function(item){
+      console.log("'" + item + "',");
+  });
+  //console.log(str);
+}
+
+if ( typeof window !== 'undefined' ) {
+  var prop = Object.getOwnPropertyNames(window);
+  var str = JSON.stringify(prop,'','\n');
+  prop.map(function(item){
+      console.log("'" + item + "',");
+  });
+  //console.log(str);
+}
+//*/
+
+
